38752a3d5d5c986d06c7e4fc7f90280ef126b5b9
jdk-web <60728267+jdk-web@users.noreply.github.com>
2021-06-17 10:47:21 +0900

    Add files via upload

---
+++ usestate을 이용한 counter/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet,View } from 'react-native';
+import Counter from './src/components/counter'
+
+const App = () => {
+  return (
+    <View>
+      <Counter />
+    </View>
+  );
+};
+
+export default App;
